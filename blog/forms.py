036e9aac4687cf9ec58942c3ce9cd51e5103f8e5
Abhay1609 <113366849+Abhay1609@users.noreply.github.com>
2022-11-16 07:17:23 +0530

    Delete blog directory

--- blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Post
-class blogform(ModelForm):
-    class Meta:
-        model= Post
-        fields= ['user','title','content','date_posted']
-
